efe891d1631eed8e5ba211b70fe1941a40dc43de
Ahmii1122 <mashup565@gmail.com>
2025-08-12 18:28:04 +0500

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -10,19 +10,55 @@ const isValid = (username)=>{ //returns boolean
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+ const validateduser = users.filter(user =>{
+    return user.username ===username && user.password === password;
+ })
+ if(validateduser.length > 0)
+  return true;
+else
+return false;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  if(!username || !password)
+    return res.status(404).json({message: "Error logging in"});
+
+  if(authenticatedUser(username,password)){
+    let accesstoken = jwt.sign({
+        data:password
+    },"access",{expiresIn:60*60});
+  req.session.authorization = {
+    accesstoken,username
+  }
+  return res.status(200).send("user logged in sccesfully");
+}
+
+  else{
+  return res.status(208).json({ message: "Invalid Login. Check username and password" });
+  }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  if(!req.session.authorization.username){
+    return res.status(401).json({message:"Please login first"})
+  }
+  const username = req.session.authorization.username;
+  const isbn = req.params.isbn
+  const review = req.params.review
+
+  const book = books[isbn];
+  if(!book){
+    return res.status(404).json({message:"no book found"})
+  }
+  const isUpdate = Object.hasOwn(book.reviews, username);
+book.reviews[username] = review.trim();
+return res
+    .status(isUpdate ? 200 : 201)
+    .json({ message: isUpdate ? "Review updated" : "Review added", reviews: book.reviews });
 });
 
 module.exports.authenticated = regd_users;
